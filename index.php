53bbd6ad4280cac9b58e0553405465c2cab58f9c
BeholdTheDevil <antdeg@outlook.com>
2018-02-13 15:16:55 +0100

    Created a base structure
    Learned that wordpress is cancerous and shouldn't be used.

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Anton Degerfeldt">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+        <title>Fabriksträdgården</title>
+        <link rel="stylesheet" type="text/css" href="css/stylesheet.css">
+        <link rel="shortcut icon" type="x-icon" href="images/LogoColored-Köpmanholmen.png">
+        <link href="https://fonts.googleapis.com/css?family=Cabin" rel="stylesheet">
+    </head>
+    <body>
+        <div class="sidebar-container">
+            <div class="logo-container">
+                <div class="logo">
+
+                </div>
+            </div>
+            <div class="category-container">
+
+            </div>
+        </div>
+        <div class="main-container">
+            <div class="primary-container">
+
+            </div>
+            <div class="secondary-container">
+                <p>Test</p>
+            </div>
+        </div>
+    </body>
+</html>
